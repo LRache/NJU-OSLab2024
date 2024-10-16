66d5b5c5db67dce79007d27e3d32899f583a7cf2
rache <2746324475@qq.com>
2024-10-16 19:45:35 +0800

    L0 working

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -15,6 +15,15 @@ static inline void puts(const char *s) {
   for (; *s; s++) putch(*s);
 }
 
+static void wait_for_esc() {
+  AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
+  ioe_read(AM_INPUT_KEYBRD, &event);
+  if (event.keycode == AM_KEY_ESCAPE && event.keydown) {
+    puts("ESC pressed, exiting...\n");
+    halt(0);
+  }
+}
+
 void print_key() {
   AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
   ioe_read(AM_INPUT_KEYBRD, &event);
@@ -64,7 +73,8 @@ int main(const char *args) {
 
   puts("Press any key to see its key code...\n");
   while (1) {
-    print_key();
+    wait_for_esc();
+    splash();
   }
   return 0;
 }
